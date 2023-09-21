7ec80a1d69c0fe7b90dbf542bc854555b74b15a3
Prabhat Sachdeva <prabhatexit0@gmail.com>
2023-09-21 12:35:15 +0530

    create navbar component

---
+++ app/components/navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+    return <div className="flex justify-center items-center p-4">
+        <h1 className="font-bold text-2xl">
+            Product List
+        </h1>
+    </div>
+}
