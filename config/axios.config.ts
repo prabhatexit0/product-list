2cae6c11a0cc0e896af255ccedb977615290655c
Prabhat Sachdeva <prabhatexit0@gmail.com>
2023-09-21 12:33:40 +0530

    create an axios config

---
+++ config/axios.config.ts
@@ -0,0 +1,10 @@
+import { default as axiosPackage } from 'axios'
+
+const axios = axiosPackage.create({
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+
+export default axios
